8c2d4ba5a1cf2a515f12732ce0950b946f00bb8d
phamtho123 <thopham.21082001@gmail.com>
2021-11-20 09:57:29 +0700

    binding

--- app/src/main/java/com/cuoiky/coffee3ae/view/Activities/DisplayCategoryActivity.java
+++ app/src/main/java/com/cuoiky/coffee3ae/view/Activities/DisplayCategoryActivity.java
@@ -1,14 +1,22 @@
 
 package com.cuoiky.coffee3ae.view.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.cuoiky.coffee3ae.R;
+import com.cuoiky.coffee3ae.databinding.DisplayCategoryLayoutBinding;
 import com.cuoiky.coffee3ae.model.LoaiMon;
 import com.cuoiky.coffee3ae.viewmodel.AdapterDisplayCategory;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -17,11 +25,40 @@ public class DisplayCategoryActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private ArrayList<LoaiMon> dataArrayList;
     private AdapterDisplayCategory dataAdapter;
+    private DisplayCategoryLayoutBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_category_layout);
+
+        binding = DisplayCategoryLayoutBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+
+        dataArrayList = new ArrayList<LoaiMon>();
+        dataAdapter = new AdapterDisplayCategory(dataArrayList);
+
+        binding.gvCategory.setLayoutManager(new LinearLayoutManager(this));
+        binding.gvCategory.setAdapter(dataAdapter);
+
+        databaseReference = FirebaseDatabase.getInstance("https://coffee3ae-default-rtdb.asia-southeast1.firebasedatabase.app/LoaiMon/").getReference();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot item:snapshot.getChildren())
+                {
+                    LoaiMon data = item.getValue(LoaiMon.class);
+                    dataArrayList.add(data);
+                    dataAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
