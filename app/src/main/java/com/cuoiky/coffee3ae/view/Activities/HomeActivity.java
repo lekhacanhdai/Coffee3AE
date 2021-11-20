3e8d14456cedd919e1dc9587dadfce385ef86d08
lekhacanhdai <khacdai0801@gmail.com>
2021-11-20 18:29:58 +0700

    thiet ke thanh navigationbar

--- app/src/main/java/com/cuoiky/coffee3ae/view/Activities/HomeActivity.java
+++ app/src/main/java/com/cuoiky/coffee3ae/view/Activities/HomeActivity.java
@@ -1,18 +1,61 @@
 package com.cuoiky.coffee3ae.view.Activities;
 
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.cuoiky.coffee3ae.R;
+import com.cuoiky.coffee3ae.databinding.ActivityHomeBinding;
+import com.cuoiky.coffee3ae.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+    private ActivityHomeBinding binding;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.tbar_home);
+//        View view = binding.navHome.getHeaderView(0);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,binding.homeLayout, binding.tbarHome
+                ,R.string.opentoggle,R.string.closetoggle){
+            @Override
+            public void onDrawerOpened(View drawerView) {    super.onDrawerOpened(drawerView); }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {    super.onDrawerClosed(drawerView); }
+        };
+
+        binding.homeLayout.addDrawerListener(drawerToggle);
+
+        drawerToggle.syncState();
+        binding.navHome.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
