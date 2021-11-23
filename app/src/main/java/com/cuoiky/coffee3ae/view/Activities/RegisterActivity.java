962296c99c639d2604d391a083caf81c3b916c65
Ngoc Dat Luong <ngocdat1908@gmail.com>
2021-11-23 19:12:23 +0700

    test

---
+++ app/src/main/java/com/cuoiky/coffee3ae/view/Activities/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.cuoiky.coffee3ae.view.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
---
+++ app/src/main/java/com/cuoiky/coffee3ae/view/Activities/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.cuoiky.coffee3ae.view.activities;
+
+import static com.cuoiky.coffee3ae.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.cuoiky.coffee3ae.R;
+import com.cuoiky.coffee3ae.databinding.SplashLayoutBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private SplashLayoutBinding binding;
+
+    private static int SPLASH_TIMER = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = SplashLayoutBinding.inflate(getLayoutInflater());
+        View rootView = binding.getRoot();
+        setContentView(rootView);
+
+        //Lấy đối tượng
+        Animation sideAnim = AnimationUtils.loadAnimation(this, anim.side_anim);
+        Animation bottomAnim = AnimationUtils.loadAnimation(this, anim.bottom_anim);
+
+        //Thiết lập animation cho component
+        binding.imgLogo.setAnimation(sideAnim);
+        binding.txtCoffeeshop.setAnimation(sideAnim);
+        binding.txtPowered.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, anim.fade_out);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
